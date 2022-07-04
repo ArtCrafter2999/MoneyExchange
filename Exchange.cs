3ee05137c678330a6723784e46d90c1a50f2cac2
ArtCrafter2999 <92592800+ArtCrafter2999@users.noreply.github.com>
2022-07-04 18:41:25 +0300

    AddProject

---
+++ Exchange.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Globalization;
+
+namespace WpfApp1
+{
+    public class Rate
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Relatively {get => "UAH"; }
+        public decimal Buy { get; set; }
+        public decimal Sell { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+        public Rate(string code, string buy, string sell)
+        {
+            Code = code;
+            Buy = decimal.Parse(buy, CultureInfo.InvariantCulture);
+            Sell = decimal.Parse(sell, CultureInfo.InvariantCulture);
+            switch (Code)
+            {
+                case "CAD":
+                    Name = "Канадский доллар";
+                    break;
+                case "CHF":
+                    Name = "Швейцарский франк";
+                    break;
+                case "CZK":
+                    Name = "Чешская крона";
+                    break;
+                case "GBP":
+                    Name = "Фунт стерлингов Велико­британии";
+                    break;
+                case "ILS":
+                    Name = "Израильский шекель";
+                    break;
+                case "JPY":
+                    Name = "Японская йена";
+                    break;
+                case "NOK":
+                    Name = "Норвежская крона";
+                    break;
+                case "PLZ":
+                    Name = "Польский злотый";
+                    break;
+                case "SEK":
+                    Name = "Шведская крона";
+                    break;
+                case "EUR":
+                    Name = "Евро";
+                    break;
+                case "RUR":
+                    Name = "Российский рубль";
+                    break;
+                case "USD":
+                    Name = "Доллар США";
+                    break;
+                case "UAH":
+                    Name = "Украинская гривна";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    public class Exchange
+    {
+        public List<Rate> ExchangeRates { get; set; } = new List<Rate>();
+        private readonly Regex _exchangeRegex = new Regex("ccy=\"([^\"]+)[^>]+buy=\"([^\"]+)[^>]+sale=\"([^\"]+)");
+        public Rate GetRate(string Code)
+        {
+            return ExchangeRates.Find(o => o.Code == Code);
+        }
+        public decimal GetModifier(Rate Rate1, Rate Rate2)
+        {
+            return Rate1.Buy / Rate2.Sell;
+        }
+        public void Renew()
+        {
+            ExchangeRates.Clear();
+            ExchangeRates.Add(new Rate("UAH", "1", "1"));
+            WebClient Link = new WebClient();
+            string DownloadString = Link.DownloadString(@"https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=3") + Link.DownloadString(@"https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=4");
+            var matches = _exchangeRegex.Matches(DownloadString);
+            foreach (Match match in matches)
+            {
+                if (GetRate(match.Groups[1].Value) == null)
+                {
+                    if (match.Groups[1].Value != "BTC")
+                    {
+                        Rate newRate;
+                        newRate = new Rate(match.Groups[1].Value, match.Groups[2].Value, match.Groups[3].Value);
+                        ExchangeRates.Add(newRate);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public Exchange Exchange { get; set; } = new Exchange();
+        public ObservableCollection<Rate> ExchangeRates => new ObservableCollection<Rate>(Exchange.ExchangeRates);
+        public ICommand Renew => new RelayCommand(o => { Exchange.Renew(); UpdateDate = $"(Обновленно {DateTime.Now:dd.MM.yyyy HH:mm:ss})"; OnPropertyChanged("ExchangeRates"); OnPropertyChanged("UpdateDate"); });
+        public string UpdateDate { get; set; } = "";
+        private Rate _leftRate;
+        public Rate comboBoxLeft
+        {
+            get => _leftRate;
+            set
+            {
+                _leftRate = value;
+                RightNum = RightNum;
+                OnPropertyChanged("comboBoxLeft");
+                OnPropertyChanged("LeftNum");
+                OnPropertyChanged("RightNum");
+                
+            }
+        }
+        private Rate _rightRate;
+        public Rate comboBoxRight
+        {
+            get => _rightRate;
+            set
+            {
+                _rightRate = value;
+                LeftNum = LeftNum;
+                OnPropertyChanged("comboBoxRight");
+                OnPropertyChanged("LeftNum");
+                OnPropertyChanged("RightNum");
+            }
+        }
+        private decimal _left;
+        public decimal LeftNum
+        {
+            get
+            {
+                return _left;
+            }
+            set
+            {
+                _left = value;
+                _right = Math.Round(_left * Exchange.GetModifier(_leftRate, _rightRate), 2);
+                OnPropertyChanged("LeftNum");
+                OnPropertyChanged("RightNum");
+            }
+        }
+        private decimal _right;
+        public decimal RightNum
+        {
+            get
+            {
+                return _right;
+            }
+            set
+            {
+                _right = value;
+                _left = Math.Round(_right * Exchange.GetModifier(_rightRate, _leftRate), 2);
+                OnPropertyChanged("LeftNum");
+                OnPropertyChanged("RightNum");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Renew.Execute(new object());
+            //UpdateDate.Content = $"(Обновленно {DateTime.Now:dd.MM.yyyy HH:mm:ss})";
+            DataContext = this;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
